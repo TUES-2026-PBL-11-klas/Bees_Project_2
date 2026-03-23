3f045b3e0e11024568dbca3f1be5e511021dd49e
Radoslav <radoslav.s.slavov@gmail.com>
2026-03-23 16:21:57 +0200

    Addid the docker files needed to build a container and run the app though docker

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from src.infrastructure.database.database import init_db, close_db
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI(title="ClearWake Routing")
 
@@ -10,3 +12,10 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     close_db()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/map", response_class=HTMLResponse)
+async def map_view():
+    with open("static/map.html") as f:
+        return f.read()
